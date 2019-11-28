5a9cbc993784d41da4accdd89e66fb98acdc7cd2
Eero Vilpponen <eero.vilpponen@gmail.com>
2019-11-28 13:58:25 +0200

    Wrong ID typee

--- dist/index.js
+++ dist/index.js
@@ -92,7 +92,7 @@ async function run() {
 
     const reposistoryId = response['data']['repository']['id'];
 
-    query = 'mutation($reposistoryId:Int!, $base:String!, $head:String!) {mergeBranch(input:{repositoryId:$reposistoryId, base:$base, head:$head}) {mergeCommit {id}}}';
+    query = 'mutation($reposistoryId:ID!, $base:String!, $head:String!) {mergeBranch(input:{repositoryId:$reposistoryId, base:$base, head:$head}) {mergeCommit {id}}}';
     variables = { reposistoryId, base: target_branch, head: commit_sha };
 
     response = await fetch('https://api.github.com/graphql', {
