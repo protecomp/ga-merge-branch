2732ca9a9a8c75b6f11ab2f42e60d34560bbf9b9
Eero Vilpponen <eero.vilpponen@gmail.com>
2019-11-28 13:34:10 +0200

    Functionality

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 const core = require('@actions/core');
-
+const fetch = require('node-fetch');
 
 // most @actions toolkit packages have async methods
 async function run() {
   try { 
-    const source_branch = core.getInput('source_branch');
+    const commit_sha = core.getInput('commit_sha');
     const target_branch = core.getInput('target_branch');
-    console.log(`Merging ${source_branch} to ${target_branch}`);
+    const github_token = core.getInput('github_token');
+    const repository = core.getInput('repository');
+    console.log(`Merging ${commit_sha} to ${repository}/${target_branch}`);
+    console.log("");
 
+    let query = 'query($owner:String!, $name:String!){repository(owner: $owner, name: $name) {id}}';
+    let variables = { owner: repository.split("/")[0], name: repository.split("/")[1] };
+
+    let response = await fetch('https://api.github.com/graphql', {
+      method: 'POST',
+      body: JSON.stringify({query, variables}),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': `bearer ${github_token}`,
+      }
+    });
+
+    const reposistoryId = response.json()['data']['repository']['id'];
+
+    query = 'mutation($reposistoryId:Int!, $base:String!, $head:String!) {mergeBranch(input:{repositoryId:$reposistoryId, base:$base, head:$head}) {}';
+    variables = { reposistoryId, base: target_branch, head: commit_sha };
+
+    response = await fetch('https://api.github.com/graphql', {
+      method: 'POST',
+      body: JSON.stringify({query}),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': `bearer ${github_token}`,
+      }
+    });
+    console.log(response.json());
   } 
   catch (error) {
     core.setFailed(error.message);
